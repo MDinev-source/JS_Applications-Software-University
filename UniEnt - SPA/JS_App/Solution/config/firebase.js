c4380ad6640a3378857420bb374939c7261ec0ba
MDinev-source <64278923+MDinev-source@users.noreply.github.com>
2021-03-20 19:01:40 +0200

    App Almost Done

---
+++ UniEnt - SPA/JS_App/Solution/config/firebase.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyBbJPSHAjJvuIll_LQG6TSglNS_AWnVGVw",
+  authDomain: "unient-b7373.firebaseapp.com",
+  projectId: "unient-b7373",
+  storageBucket: "unient-b7373.appspot.com",
+  messagingSenderId: "1046671666405",
+  appId: "1:1046671666405:web:dac6be820552640787780c",
+  measurementId: "G-C5VB2GZ8PT"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
