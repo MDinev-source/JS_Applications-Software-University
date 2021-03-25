1b7dee66472d4da3e86baf2c7ead2babc989141d
MDinev-source <64278923+MDinev-source@users.noreply.github.com>
2021-03-25 21:29:43 +0200

    Run Register Function

--- Movies - SPA/JS_App/app.js
+++ Movies - SPA/JS_App/app.js
@@ -1,11 +1,16 @@
 import { getHome } from "./controller/home.js";
-import { getLogin } from "./controller/user.js";
+import { getLogin, getRegister, postLogin, postRegister } from "./controller/user.js";
 
-const app = Sammy("body", function(){
+const app = Sammy("body", function () {
     this.use("Handlebars", "hbs");
 
     this.get('#/home', getHome);
 
+    this.get('#/register', getRegister);
+    this.post('#/register', postRegister);
+
     this.get('#/login', getLogin);
+    this.post('#/login', postLogin);
 });
-app.run ('#/home');
+
+app.run('#/home');
--- Movies - SPA/JS_App/controller/user.js
+++ Movies - SPA/JS_App/controller/user.js
@@ -1,7 +1,36 @@
-import { setHeader } from './auth.js'
+import { login,registerUser } from '../models/user.js';
+import { saveUserInfo, setHeader } from './auth.js'
 import commonPartial from './partials.js'
 
-export function getLogin(ctx){
+export function getRegister(ctx) {
+    setHeader(ctx)
+    ctx.loadPartials(commonPartial).partial('./view/user/register.hbs')
+}
+
+export function postRegister(ctx) {
+    const { email, password, repeatPassword } = ctx.params;
+    if (password !== repeatPassword) {
+        throw new Error('Passwords do not match!');
+    }
+    registerUser(email, password)
+        .then(res => {
+            saveUserInfo(res.user.email)
+            ctx.redirect('#/home');
+        })
+        .catch(e => console.log(e));
+}
+
+export function getLogin(ctx) {
     setHeader(ctx);
     ctx.loadPartials(commonPartial).partial('./view/user/login.hbs');
+}
+
+export function postLogin(ctx) {
+    const { email, passord } = ctx.params;
+
+    login(email, passord)
+        .then(res => {
+            saveUserInfo(res.user.email);
+            ctx.redirect('#/home');
+        }).catch(e => console.log(e));
 }
